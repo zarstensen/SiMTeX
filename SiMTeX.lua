1c8a2ffa04617c9d6e0aba73fb3ad0a70e277a51
Zarstensen <61250140+zarstensen@users.noreply.github.com>
2024-12-12 09:12:36 +0100

    Begin implementation of code inclusion

--- SiMTeX.lua
+++ SiMTeX.lua
@@ -1,9 +1,13 @@
 local P = {}
 
-P.smart_matrix_separator = ','
-P.smart_matrix_env_stack = {}
+-- Smart Matrix
 
-function P.SmartMatrix(rows, columns, contents, brackets)
+local SMat = {}
+
+SMat.smart_matrix_separator = ','
+SMat.smart_matrix_env_stack = {}
+
+function SMat.SmartMatrix(rows, columns, contents, brackets)
     columns = tonumber(columns)
     rows = tonumber(rows)
 
@@ -20,15 +24,15 @@ function P.SmartMatrix(rows, columns, contents, brackets)
 
     if columns and rows and columns * rows ~= #elems then
         tex.error(string.format("Matrix was [%sx%s] needing [%s] elements, but [%s] elements were given",
-                rows, columns, rows * columns, #elems))
+            rows, columns, rows * columns, #elems))
         return
     elseif columns and #elems % columns ~= 0 then
         tex.error(string.format("Matrix was [*x%s] needing a multiple of [%s] elements, but [%s] elements were given",
-                columns, columns, #elems))
+            columns, columns, #elems))
         return
     elseif rows and #elems % rows ~= 0 then
         tex.error(string.format("Matrix was [%sx*] needing a multiple of [%s] elements, but [%s] elements were given",
-                rows, rows, #elems))
+            rows, rows, #elems))
         return
     end
 
@@ -36,21 +40,21 @@ function P.SmartMatrix(rows, columns, contents, brackets)
 
     if columns then
         rows = #elems / columns
-    elseif rows then   
+    elseif rows then
         columns = #elems / rows
     end
 
     local matrix_env = "matrix"
 
-    if #P.smart_matrix_env_stack > 0 then
-        matrix_env = P.smart_matrix_env_stack[#P.smart_matrix_env_stack]
+    if #SMat.smart_matrix_env_stack > 0 then
+        matrix_env = SMat.smart_matrix_env_stack[#SMat.smart_matrix_env_stack]
     end
 
-    local latex_out = { }
+    local latex_out = {}
     table.insert(latex_out, string.format("\\begin{%s%s}", brackets, matrix_env))
-    
-    for r=0, rows - 1 do
-        for c=0, columns - 1 do 
+
+    for r = 0, rows - 1 do
+        for c = 0, columns - 1 do
             table.insert(latex_out, elems[r * columns + c + 1])
 
             if c ~= columns - 1 then
@@ -68,13 +72,92 @@ function P.SmartMatrix(rows, columns, contents, brackets)
     tex.print(table.concat(latex_out))
 end
 
-function P.pushSmartMatEnv(env)
-    table.insert(P.smart_matrix_env_stack, env)
+function SMat.pushSmartMatEnv(env)
+    table.insert(SMat.smart_matrix_env_stack, env)
 end
 
-function P.popSmartMatEnv()
-    if #P.smart_matrix_env_stack > 0 then
-        table.remove(P.smart_matrix_env_stack, #P.smart_matrix_env_stack)
+function SMat.popSmartMatEnv()
+    if #SMat.smart_matrix_env_stack > 0 then
+        table.remove(SMat.smart_matrix_env_stack, #SMat.smart_matrix_env_stack)
+    end
+end
+
+P.SMat = SMat
+
+-- Code Inclusions
+
+local CodeInc = {}
+
+function CodeInc.extractCStyleScope(code, scope_start)
+    
+    local line_start, line_end = CodeInc.findLineWithExpr(code, scope_start)
+
+    if line_start == nil then
+        error(("Could not find any line containint '%s'"):format(line_start))
+    end
+
+    -- now search for the first curly bracket, this will mark the start of the scope.
+
+    local scope_depth = -1
+    local scope_start = nil
+    local scope_end = nil
+
+    for i=line_start, #code do
+        if code[i] == '{' then
+            scope_depth = scope_depth + 1
+            scope_start = i
+            break
+        end
+    end
+
+    if scope_depth then
+        error(("Could not find a scope starter ({) after character no. %s"):format(line_start))
+    end
+
+    -- now continue until we go out of scope
+
+    local is_inside_string = false
+
+    for i=scope_start+1, #code do
+
+        if code[i] == '{' then
+            scope_depth = scope_depth + 1
+        elseif code[i] == '}' then
+            scope_depth = scope_depth - 1
+        end
+
+        if scope_depth < 0 then
+            scope_end = i
+            break
+        end
+    end
+
+    return code:sub(scope_start, scope_end)
+end
+
+-- Returns the starting and ending index of the line, where the given expression is present in the passed string
+-- Returns nil if no such line could be found.
+function CodeInc.findLineWithExpr(text, expr)
+    local current_index = 1
+
+    -- Loop through each line of the passed string
+    while true do
+        local newline_index = text:find("\n", 1, true)
+
+        if newline_index ~= nil then
+            local line = text:sub(1, newline_index)
+            local found_expr_index = line:find(expr, 1, true)
+            if found_expr_index ~= nil and found_expr_index == 1 then
+                return current_index, current_index + #line - 2
+            else
+                current_index = current_index + #line
+                text = text:sub(newline_index + 1)
+            end
+        elseif text:find(expr, 1, true) then
+            return current_index, #expr
+        else
+            return nil
+        end
     end
 end
 
